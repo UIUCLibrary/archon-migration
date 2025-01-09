2f832d28cc162e565901be15a18f19430ff600e8
graykr <graykr@users.noreply.github.com>
2025-01-09 15:00:17 -0600

    create CustomArchonLocationMapper class

---
+++ src/org/nyu/edu/dlts/utils/CustomArchonLocationMapper.java
@@ -0,0 +1,92 @@
+package org.nyu.edu.dlts.utils;
+
+import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+/**
+ * Class to provide custom location information mapping information
+ */
+
+public class CustomArchonLocationMapper {
+
+    private JSONObject archonLocationsMap;
+    private String archonSectionSeparator = "";
+    private String locationMapFileName = "";
+
+    /**
+     * Default constructor 
+     */
+    public CustomArchonLocationMapper(){
+        setLocationMappingVariables("default");
+        loadLocationMapping();
+    }
+    
+    /**
+     * Constructor using custom option indicator
+     * Supplies the section separator and file name for each option
+     * @param customOption
+     */
+    public CustomArchonLocationMapper(String customOption){
+        setLocationMappingVariables(customOption);
+        loadLocationMapping();
+    }
+
+    /**
+     * Method to set the file name and section separator variables based on data in a json file
+     * @param optionCode
+     */
+    private void setLocationMappingVariables(String optionCode){
+        //load json
+        try {
+            String text = IOUtils.toString(this.getClass().getResourceAsStream("custom-location-info.json"), "UTF-8");
+            JSONObject customInfo = new JSONObject(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        //set variables based on json
+        if(customInfo.has(optionCode)) {
+            JSONObject optionCodeJS = customInfo.getJSONObject(optionCode);
+            if(optionCodeJS.has("FileName")) locationMapFileName = optionCodeJS.getString("FileName");
+            if(optionCodeJS.has("SectionSeparator")) archonSectionSeparator = optionCodeJS.getString("SectionSeparator");
+        }
+    }
+    
+    /**
+     * Method to load the location mappings
+     * To be called after the location map file name has been set
+     */
+    private void loadLocationMapping(){
+        if(locationMapFileName != null && locationMapFileName != ""){
+            try {
+                String text = IOUtils.toString(this.getClass().getResourceAsStream(locationMapFileName), "UTF-8");
+                archonLocationsMap = new JSONObject(text);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    /**
+     * Method to get the locations map
+     * @return
+     */
+    public HashMap getLocationsMap(){
+        return archonLocationsMap;
+    }
+
+    /**
+     * Method to get the section shelf separator
+     * @return
+     */
+    public String getSectionSeparator(){
+        return sectionShelfSeparator;
+    }
+}
