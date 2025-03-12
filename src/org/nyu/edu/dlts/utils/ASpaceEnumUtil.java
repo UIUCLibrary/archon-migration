8d332a12f9f615fc776c9bea58f5dd219a34a430
graykr <graykr@users.noreply.github.com>
2025-03-12 17:18:57 -0500

    map processing statuses to ASpace defaults to avoid redundancy

--- src/org/nyu/edu/dlts/utils/ASpaceEnumUtil.java
+++ src/org/nyu/edu/dlts/utils/ASpaceEnumUtil.java
@@ -468,7 +468,18 @@ public class ASpaceEnumUtil {
      */
     public String getASpaceCollectionManagementRecordProcessingPriority(int arID) {
         String key = "processing_priority_" + arID;
-        return getEnumValueForID(key, "low");
+        String arValue = getEnumValueForID(key, "low");
+        if (arValue.contains("high")) {
+            return ASpaceCollectionManagementRecordEnums[0];
+        } else if(arValue.contains("medium")) {
+            return ASpaceCollectionManagementRecordEnums[1];
+        } else if(arValue.contains("low")) {
+            return ASpaceCollectionManagementRecordEnums[2];
+        } else if(returnATValue) {
+            return arValue;
+        } else {
+            return UNMAPPED;
+        }
     }
 
     /**
