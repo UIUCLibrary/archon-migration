a4c9be6ab5b3d5e25ed3810d2d5c0679676a5083
graykr <graykr@users.noreply.github.com>
2025-03-26 18:26:27 -0500

    change accession date type from "other" to "creation"

--- src/org/nyu/edu/dlts/utils/ASpaceMapper.java
+++ src/org/nyu/edu/dlts/utils/ASpaceMapper.java
@@ -637,7 +637,7 @@ public class ASpaceMapper {
         }
 
         // add the inclusive dates
-        addDate(record.getString("InclusiveDates"), json, "inclusive", "other");
+        addDate(record.getString("InclusiveDates"), json, "inclusive", "creation");
 
         // add the collection management record now
         if(record.has("ExpectedCompletionDate") && !record.getString("ExpectedCompletionDate").isEmpty()) {
