941f77f9b840e66c0c37475579e205b53b01dff4
graykr <graykr@users.noreply.github.com>
2025-05-05 15:27:18 -0500

    fix: do not add default "image-master" use statement for digital objects
    Since the use statement field is not required and is not something that maps well to archon data, do not add it to the digital object records migrated.

--- src/org/nyu/edu/dlts/utils/ASpaceMapper.java
+++ src/org/nyu/edu/dlts/utils/ASpaceMapper.java
@@ -849,7 +849,7 @@ public class ASpaceMapper {
             JSONObject fileVersionJS = new JSONObject();
 
             fileVersionJS.put("file_uri", record.getString("ContentURL"));
-            fileVersionJS.put("use_statement", "image-master");
+            //fileVersionJS.put("use_statement", "image-master");
             fileVersionJS.put("xlink_actuate_attribute", "none");
             fileVersionJS.put("xlink_show_attribute", "none");
 
@@ -859,7 +859,7 @@ public class ASpaceMapper {
 
             JSONObject fileVersionJS = new JSONObject();
             fileVersionJS.put("file_uri", digitalObjectBaseURI + filename);
-            fileVersionJS.put("use_statement", "image-master");
+            //fileVersionJS.put("use_statement", "image-master");
             fileVersionJS.put("xlink_actuate_attribute", "none");
             fileVersionJS.put("xlink_show_attribute", "none");
             fileVersionJS.put("file_format_name", record.get("FileTypeID"));
