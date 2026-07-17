d373d846dbd8ebbee79bd1f19bd801a22ae7201e
graykr <graykr@users.noreply.github.com>
2026-07-17 10:24:01 -0500

    print additional information (counts of records) for testing

--- src/org/nyu/edu/dlts/utils/ArchonRecordInspector.java
+++ src/org/nyu/edu/dlts/utils/ArchonRecordInspector.java
@@ -262,6 +262,7 @@ public class ArchonRecordInspector {
                 e.printStackTrace();
             }
         }
+        System.out.println("collection record list length: " + collectionRecordsJS.length());
     }
 
    /**
@@ -404,7 +405,10 @@ public class ArchonRecordInspector {
             } catch (JSONException e) {
                 e.printStackTrace();
             }
+        } else {
+            System.out.println("Digital object " + archonID + " not found");
         }
+        System.out.println("Digital object list length: " + digitalObjectRecordsJS.length());
     }
 
     /**
@@ -466,7 +470,7 @@ public class ArchonRecordInspector {
         System.out.println("Classification hashmap size: " + testClassificationIdentifiers.size() + "\n\n");
         System.out.println("Classification parents hashmap size: " + testClassificationParents.size() + "\n\n");
         
-        String archonIDtoTest = "8753";//"7394";
+         String archonIDtoTest = "8753";//"7394";
         loadCollectionByArchonID(archonIDtoTest);
         testConvertCollection(archonIDtoTest, mapper);
         
@@ -474,7 +478,7 @@ public class ArchonRecordInspector {
         loadCreatorByArchonID(archonCreatorIDtoTest);
         testConvertCreator(archonCreatorIDtoTest, mapper);
         
-        String archonDigitalIDtoTest = "3540";//"188";
+        String archonDigitalIDtoTest = "18979";//"916";//"3540";//"188";
         testConvertDigitalObject(archonDigitalIDtoTest, mapper);
 
         String archonAccesionIDtoTest = "142";
